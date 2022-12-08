c0bbb72fee33227b8b66e8cf2b8765712044d02e
madcow95 <lobasketve@naver.com>
2022-12-08 22:23:23 +0900

    Header 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <Navbar bg="dark" variant="dark">
+          <Container>
+              <Navbar.Brand href="#home">SHOP</Navbar.Brand>
+              <Nav className="me-auto">
+                  <Nav.Link href="/">홈</Nav.Link>
+                  <Nav.Link href="/login">로그인</Nav.Link>
+                  <Nav.Link href="/join">회원가입</Nav.Link>
+              </Nav>
+          </Container>
+      </Navbar>
+    </div>
   );
 }
 
